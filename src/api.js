2f0010c1af74915bb8511aa40927c4322918630d
DominiqueNix <dominiquenix@google.com>
2023-11-21 15:49:21 -0600

    updated apiURL setting

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ let apiURL;
 
 if(process.env.NODE_ENV === 'development'){
     apiURL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+} else if(process.env.NODE_ENV === 'production'){
+    apiURL = process.env.REACT_APP_API_URL
 } else {
     apiURL = ''
 }
